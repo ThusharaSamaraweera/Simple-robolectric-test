a6498a8d9ca6691c0ac324c3d96c593c191a8e6b
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-05-09 05:53:11 +0530

    add testing text on fragment

--- app/src/test/java/com/example/robolectrictest/ExampleUnitTest.kt
+++ app/src/test/java/com/example/robolectrictest/ExampleUnitTest.kt
@@ -1,17 +1,42 @@
 package com.example.robolectrictest
+import android.widget.TextView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
 
 import org.junit.Test
+import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+@RunWith(RobolectricTestRunner::class)
+class ExampleUnitTestTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun test_text_on_activity() {
+        val activity = Robolectric.buildActivity(MainActivity::class.java).create().get()
+        val text : TextView = activity.findViewById<TextView>(R.id.main_activity_text)
+        assertEquals("Hello World!", text.text)
+    }
+
+    @Test
+    fun test_text_on_fragment(){
+        val fragment = FirstFragment()
+
+        val activity = Robolectric.buildActivity((
+                SecondActivity::class.java
+                ))
+            .create()
+            .start()
+            .resume()
+            .get()
+
+        val fragmentManager: FragmentManager = activity.supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(fragment, null)
+        fragmentTransaction.commit()
+
+        val textView : TextView = activity.findViewById<TextView>(R.id.tv_hello_fragment)
+        assertEquals("hello fragment", textView.text)
     }
 }
